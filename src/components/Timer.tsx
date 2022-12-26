f3b3f7abd06d8bfa7962bb1792430d45fc7ac3d0
Denis Karpenko <denis_swd@mail.ru>
2022-12-26 21:41:29 +0100

    feat: button for start timer, clear Timeout

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,17 +2,23 @@ import { FC, useEffect, useState } from 'react'
 import s from './Timer.module.scss'
 
 export const Timer: FC = () => {
-  const [value, setValue] = useState(120000)
+  const [value, setValue] = useState(0)
   useEffect(() => {
-    setTimeout(() => {
+    const time = setTimeout(() => {
       setValue(value > 0 ? value - 1000 : 0)
+      return () => clearTimeout(time)
     }, 1000)
   }, [value])
+
   const min = Math.floor(value / 60000)
   const sec = value % 60000 / 1000
+  const onStartTimer = () => {
+    setValue(value + 120000)
+  }
   return (
     <div className={s.timerWrap}>
       Timer: {min}:{sec < 10 ? '0' + sec : sec}
+      <button onClick={() => onStartTimer()} disabled={value > 0}>start</button>
     </div>
   )
 }
